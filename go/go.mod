9f1a731bd7e04f62f7fff0957462b317f72e0089
hiRainn <wr_sorarara@yahoo.co.jp>
2020-06-24 17:59:58 +0800

    import gin

---
+++ go/go.mod
@@ -0,0 +1,5 @@
+module blog
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.6.3 // indirect
---
+++ go/main.go
@@ -0,0 +1,13 @@
+// geektutu.com
+// main.go
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, Geektutu")
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
